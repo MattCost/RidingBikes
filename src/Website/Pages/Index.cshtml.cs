d66420b4d8e212b404f85734b2448acfa7c61dca
Matt Costuros <matt.costuros@gmail.com>
2023-02-25 12:19:13 -0500

    default website

---
+++ src/Website/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly RidingBikes.APIClient.IClient _client;
+
+    public IndexModel(ILogger<IndexModel> logger, RidingBikes.APIClient.IClient client)
+    {
+        _logger = logger;
+        _client = client;
+    }
+
+    public IEnumerable<RidingBikes.APIClient.GroupRide> Rides { get; set; } = new List<RidingBikes.APIClient.GroupRide>();
+
+    public async Task OnGetAsync()
+    {
+        var result = await _client.GroupRides_GetGroupRidesAsync();
+        Rides = result;
+    }
+}
