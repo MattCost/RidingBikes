4562ea1481a6f0e0060b2b436676dc65a2d1b3f5
Matt Costuros <matt.costuros@gmail.com>
2023-02-26 15:39:49 -0500

    update helper extension method

---
+++ src/Common/UpdateExtensions.cs
@@ -0,0 +1,40 @@
+using RidingBikes.Common.Models;
+
+namespace RidingBikes.Common;
+
+public static class UpdateExtensions
+{
+    public static bool CheckForUpdate(this double value, double other)
+    {
+        if(double.IsNaN(other)) return false;
+
+        if(double.IsNaN(value)) return true;
+
+        return value != other;
+    }
+
+    public static bool CheckForUpdate(this string value, string other)
+    {
+        if(string.IsNullOrEmpty(value) && string.IsNullOrEmpty(other)) return false;
+        if(string.IsNullOrEmpty(value) && !string.IsNullOrEmpty(other)) return true;
+        return !string.Equals(value, other);
+    }
+
+    public static bool CheckForUpdate(this DateTime value, DateTime other)
+    {
+        return value != other;
+    }
+
+    public static bool CheckForUpdate(this Guid value, Guid other)
+    {
+        if(other == Guid.Empty) return false;
+        if(value == Guid.Empty) return true;
+
+        return !Guid.Equals(value, other);
+    }
+
+    public static bool CheckForUpdate(this RideType value, RideType other)
+    {
+        return value != other;
+    }
+}
