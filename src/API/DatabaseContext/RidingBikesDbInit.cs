2c8ab166ae807bcf41c86609d2366c6099bbc2d5
Matt <matt.costuros@gmail.com>
2023-05-21 16:44:16 -0400

    net7 update

--- src/API/DatabaseContext/RidingBikesDbInit.cs
+++ src/API/DatabaseContext/RidingBikesDbInit.cs
@@ -14,10 +14,10 @@ public static class DbInitializer
             var route2 = new BikeRouteModel { Id = Guid.NewGuid(), Distance = Random.Shared.NextDouble() * 20.0, RideWithGPSUrl = "Second Route" };
             var rides = new GroupRideModel[]
             {
-                GroupRideModel.Create( new GroupRideCreateModel{ DateTime = DateTime.Now, Location = "Elmwood", BikeRouteId = route1.Id, RideType = RideType.A}),
-                GroupRideModel.Create( new GroupRideCreateModel{ DateTime = DateTime.Now, Location = "Niagara", BikeRouteId = route2.Id, RideType = RideType.BSweaty}),
-                GroupRideModel.Create( new GroupRideCreateModel{ DateTime = DateTime.Now, Location = "Niagara", BikeRouteId = route2.Id, RideType = RideType.BChatty}),
-                GroupRideModel.Create( new GroupRideCreateModel{ DateTime = DateTime.Now, Location = "Gravel Gravel", BikeRouteId = Guid.Empty, RideType = RideType.D})
+                GroupRideModel.Create( new GroupRideCreateModel{ StartTime = TimeOnly.FromDateTime(DateTime.UtcNow), Location = "Elmwood", BikeRouteId = route1.Id, RideType = RideType.A}),
+                GroupRideModel.Create( new GroupRideCreateModel{ StartTime = TimeOnly.FromDateTime(DateTime.UtcNow), Location = "Niagara", BikeRouteId = route2.Id, RideType = RideType.BSweaty}),
+                GroupRideModel.Create( new GroupRideCreateModel{ StartTime = TimeOnly.FromDateTime(DateTime.UtcNow), Location = "Niagara", BikeRouteId = route2.Id, RideType = RideType.BChatty}),
+                GroupRideModel.Create( new GroupRideCreateModel{ StartTime = TimeOnly.FromDateTime(DateTime.UtcNow), Location = "Gravel Gravel", BikeRouteId = Guid.Empty, RideType = RideType.D})
             };
             context.BikeRoutes.Add(route1);
             context.BikeRoutes.Add(route2);
